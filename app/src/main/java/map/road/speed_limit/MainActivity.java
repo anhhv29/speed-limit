d41cbb25d7f3945ff38d3b9fe7990fee9ccf025b
anhhv <anhhv@gotech.vn>
2023-06-05 11:03:24 +0700

    get lat, lon from gps

--- app/src/main/java/map/road/speed_limit/MainActivity.java
+++ app/src/main/java/map/road/speed_limit/MainActivity.java
@@ -1,14 +1,65 @@
 package map.road.speed_limit;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final int MY_PERMISSIONS_REQUEST_LOCATION = 0;
+
+    TextView tvLat, tvLon;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tvLat = findViewById(R.id.tvLat);
+        tvLon = findViewById(R.id.tvLon);
+        checkLocation();
+
+    }
+
+    @SuppressLint("SetTextI18n")
+    private void checkLocation() {
+        // Kiểm tra quyền truy cập vị trí
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            // Nếu chưa được cấp quyền, yêu cầu cấp quyền
+            ActivityCompat.requestPermissions(this,
+                    new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
+                    MY_PERMISSIONS_REQUEST_LOCATION);
+        } else {
+            // Nếu đã được cấp quyền, lấy vị trí hiện tại
+            LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+            Location location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+            if (location == null) {
+                location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+            }
+            if (location != null) {
+                double latitude = location.getLatitude();
+                double longitude = location.getLongitude();
+                // Sử dụng latitude và longitude ở đây
+
+                String Lat = Double.toString(latitude);
+                String Lon = Double.toString(longitude);
+
+                Log.d("location", "lat"+Lat);
+                Log.d("location", "lon"+Lon);
+
+                tvLat.setText("Latitude (Vĩ Độ): \n" + Lat);
+                tvLon.setText("Longitude (Kinh Độ): \n" + Lon);
+            }
+        }
     }
 }
